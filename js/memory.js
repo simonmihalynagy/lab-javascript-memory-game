dfef04343d68a93126333c8e0a8709ed489eaf2e
Simon Nagy <nagysimonmihaly@gmail.com>
2021-06-14 19:54:11 +0200

    finish until shuffle

--- js/memory.js
+++ js/memory.js
@@ -2,18 +2,57 @@ class MemoryGame {
   constructor(cards) {
     this.cards = cards;
     // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
 
-  shuffleCards() {
+  shuffleCards(cards) {
     // ... write your code here
+    if (!cards) {
+      return undefined;
+    }
+
+    /* let oldElement;
+    for (let i = cards.length - 1; i > 0; i--) {
+      let rand = Math.floor(Math.random() * (i + 1));
+      oldElement = cards[i];
+      cards[i] = cards[rand];
+      cards[rand] = oldElement;
+    }
+    return cards; */
+    let currentIndex = cards.length,
+      randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+      // And swap it with the current element.
+      cards[([currentIndex], cards[randomIndex])] = [
+        cards[randomIndex],
+        cards[currentIndex]
+      ];
+    }
+
+    return cards;
   }
 
   checkIfPair(card1, card2) {
     // ... write your code here
+    this.pairsClicked += 1;
+
+    if (card1 === card2) {
+      this.pairsGuessed += 1;
+    }
+    return card1 === card2;
   }
 
   checkIfFinished() {
     // ... write your code here
+    return this.pairsGuessed * 2 === this.cards.length;
   }
 }
 
